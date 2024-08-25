6cb6a6a6ce307cbc0e2620bca1c58791db0cd9e5
Sania La <saniourry@gmail.com>
2024-08-25 15:37:30 +0330

    create colors.txt + update snake.py

--- snake.py
+++ snake.py
@@ -1,16 +1,33 @@
 import pygame as pg
+from pygame.math import Vector2
 import sys
 
 pg.init()
+bg_color = (173, 204, 96)
+size_of_cells = 25
+num_of_cells = 20
 
-screen = pg.display.set_mode((500, 500))
+
+screen = pg.display.set_mode((size_of_cells*num_of_cells, size_of_cells*num_of_cells))
 pg.display.set_caption("SNAKE GAME")
 clock = pg.time.Clock()
+
+class Food:
+    def __init__(self):
+        self.position = Vector2(5, 6)
+    def draw_food(self):
+        food = pg.Rect(self.position.x * size_of_cells, self.position.y * size_of_cells, size_of_cells, size_of_cells)
+        pg.draw.rect(screen, (30, 120, 130), food)
+
+
+food = Food()
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+    screen.fill(bg_color)
+    food.draw_food()
     pg.display.update()
     clock.tick(60)
